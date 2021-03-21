35809179d86fd01000966db0e8e9a369ad20dfd8
clayton delouth <claytondelouth48@gmail.com>
2021-03-21 11:39:26 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: "y",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "-hUyLhrz7i_FfHPDHcAUuFaX-h_wthWsV3TqUGUaeO0",
+        spaceId: "6ppe5g4v058k",
+      },
+    },
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "123456",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
